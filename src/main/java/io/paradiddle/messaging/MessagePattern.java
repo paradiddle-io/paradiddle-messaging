1c8b7ca948b7d7fc18dfedeb665e1d1554d65077
Michael Juliano <michaelj@sunsofterp.com>
2021-06-01 00:15:08 -0400

    Added copyright and a Mailbox implementation.

--- src/main/java/io/paradiddle/messaging/MessagePattern.java
+++ src/main/java/io/paradiddle/messaging/MessagePattern.java
@@ -1,3 +1,21 @@
+/*
+ * paradiddle-messaging A library that builds on OpenJDK Project Loom to provide Erlang-style message passing between processes.
+ * Copyright (C) 2021 Michael Juliano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package io.paradiddle.messaging;
 
 import java.util.function.Consumer;
